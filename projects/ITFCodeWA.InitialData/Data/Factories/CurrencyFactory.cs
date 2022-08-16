7dd7692c1fb9f4b6b7a5aa3608fd0df2a150d64d
Viacheslav <viacheslav.iakovchuk@gmail.com>
2022-08-16 14:34:18 +0300

    Commit Fixed

--- projects/ITFCodeWA.InitialData/Data/Factories/CurrencyFactory.cs
+++ projects/ITFCodeWA.InitialData/Data/Factories/CurrencyFactory.cs
@@ -4,15 +4,11 @@ namespace ITFCodeWA.InitialData.Data.Factories
 {
     internal static class CurrencyFactory
     {
-<<<<<<< HEAD
         public static CurrencyModel Create(string name, string shortName, int code) => new()
         {
             Name = name,
             ShortName = shortName,
             Code = code
         };
-=======
-        public static CurrencyModel Create() => new();
->>>>>>> 1e142695dcc42e2888d9abcfd67561ef443f92f3
     }
 }
